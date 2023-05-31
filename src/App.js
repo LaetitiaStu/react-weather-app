bbb38b914fac6649b85702e2518043bb31e82ef5
LaetitiaStu <129389279+LaetitiaStu@users.noreply.github.com>
2023-05-31 17:33:30 +0200

    Added GitHub repository

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://cool-faun-28ddd5.netlify.app/" target="_blank">
+          Laetitia Stuchtey
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/LaetitiaStu/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
