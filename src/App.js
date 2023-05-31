08a24cf981b15eb0a86363a49c27488ce3360901
LaetitiaStu <129389279+LaetitiaStu@users.noreply.github.com>
2023-05-31 17:43:46 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://cool-faun-28ddd5.netlify.app/" target="_blank">
-          Laetitia Stuchtey
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/LaetitiaStu/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://cool-faun-28ddd5.netlify.app/" target="_blank">
+            Laetitia Stuchtey
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/LaetitiaStu/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
