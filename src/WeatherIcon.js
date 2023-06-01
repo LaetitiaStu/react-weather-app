ddf038cf35e8eb043b61c324e0a630c8a299d843
LaetitiaStu <129389279+LaetitiaStu@users.noreply.github.com>
2023-06-01 08:47:43 +0200

    Display Forecast for one day

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -25,7 +25,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="#4d3770"
-      size={50}
+      size={props.size}
       animate={true}
     />
   );
